f309dffda94ea6525dc159a1a0216de4d79247b0
Coldpasta <Lmacewicz@gmail.com>
2025-03-11 14:09:30 +0100

    Rest of Put and Post functionalities added, Controller Created, app works

--- src/main/java/com/lm/footballzone/player/PlayerService.java
+++ src/main/java/com/lm/footballzone/player/PlayerService.java
@@ -1,9 +1,11 @@
 package com.lm.footballzone.player;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -38,10 +40,44 @@ public class PlayerService {
                 .filter(player -> player.getPosition().toLowerCase().contains(position.toLowerCase()))
                 .collect(Collectors.toList());
     }
-    public List<Player> getPlayersByNation(String searchedNation){
+
+    public List<Player> getPlayersByNation(String searchedNation) {
         return playerRepository.findAll().stream()
                 .filter(player -> player.getNation().toLowerCase().contains(searchedNation.toLowerCase()))
                 .collect(Collectors.toList());
     }
 
+    public List<Player> getPlayerByTeamAndPosition(String teamName, String position) {
+        return playerRepository.findAll().stream()
+                .filter(player -> teamName.equals(player.getTeamName()) && position.equals(player.getPosition()))
+                .collect(Collectors.toList());
+    }
+
+    public Player addPlayer(Player player) {
+        playerRepository.save(player);
+        return player;
+    }
+
+    public Player updatePlayer(Player updatedPlayer) {
+        Optional<Player> exsistingPlayer = playerRepository.findByName(updatedPlayer.getName());
+
+        if (exsistingPlayer.isPresent()) {
+            Player playertoUpdate = exsistingPlayer.get();
+            playertoUpdate.setName(updatedPlayer.getName());
+            playertoUpdate.setTeamName(updatedPlayer.getTeamName());
+            playertoUpdate.setNation(updatedPlayer.getNation());
+            playertoUpdate.setPosition(updatedPlayer.getPosition());
+
+            playerRepository.save(playertoUpdate);
+            return playertoUpdate;
+        }
+        return null;
+    }
+
+    @Transactional
+    public void deletePlayer(String name) {
+        playerRepository.deleteByName(name);
+    }
 }
+
+
